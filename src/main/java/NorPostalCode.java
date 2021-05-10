e8a13c021ee6e7216655625ff6a43c9b2c080320
Tobias Rødahl Thingnes <tobiasth@stud.ntnu.no>
2021-05-10 12:34:01 +0200

    Implemented NorPostalCode

---
+++ src/main/java/NorPostalCode.java
@@ -0,0 +1,143 @@
+import java.util.Arrays;
+
+/**
+ * Norwegian postal code.
+ *
+ * Consists of 4 symbols which all are numbers 0-9.
+ *
+ * @author trthingnes
+ */
+public class NorPostalCode implements PostalCode {
+    private final int postalCode;
+    private final String postalName;
+    private final int provinceCode;
+    private final String provinceName;
+    private final Type type;
+
+    /**
+     * Constructs a new Norwegian postal code.
+     *
+     * @param postalCode Integer postal code.
+     * @param postalName Postal code name.
+     * @param provinceCode Integer province code.
+     * @param provinceName Province name.
+     * @param type Postal code type char code.
+     * @throws IllegalArgumentException If names are empty or codes are not integers.
+     */
+    public NorPostalCode(String postalCode, String postalName, String provinceCode, String provinceName, String type) throws IllegalArgumentException {
+        if(postalName.isBlank() || provinceName.isBlank()) {
+            throw new IllegalArgumentException("Postal and province names cannot be blank.");
+        }
+
+        try {
+            this.postalCode = Integer.parseInt(postalCode);
+            this.provinceCode = Integer.parseInt(provinceCode);
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Norwegian postal and province codes have to be integers.");
+        }
+
+        this.postalName = postalName;
+        this.provinceName = provinceName;
+        this.type = Arrays.stream(Type.values())
+                .filter(t -> t.code == type.charAt(0))
+                .findAny().orElse(Type.UNKNOWN);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getAsString() {
+        return String.valueOf(postalCode);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getName() {
+        return postalName;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getProvinceCode() {
+        return String.valueOf(provinceCode);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getTypeDescription() {
+        return type.description;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getCountryCode() {
+        return "nor";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getCountryName() {
+        return "Norway";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+
+        NorPostalCode that = (NorPostalCode) other;
+
+        return postalCode == that.postalCode;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return postalCode;
+    }
+
+    /**
+     * Postal code types. Shows what the postal codes are used for.
+     */
+    public enum Type {
+        BOTH('B', "Used for both street addresses and P.O. boxes."),
+        MIX('F', "Used for a mix of services."),
+        STREET('G', "Used for street addresses."),
+        BOX('P', "Used for P.O. boxes."),
+        SERVICE('S', "Unused"),
+        UNKNOWN('*', "Unknown");
+
+        private final char code;
+        private final String description;
+
+        Type(char code, String description) {
+            this.code = code;
+            this.description = description;
+        }
+    }
+}
