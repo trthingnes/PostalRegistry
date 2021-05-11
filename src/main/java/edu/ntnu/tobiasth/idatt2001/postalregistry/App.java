2b8fc9a121a2615d97b39aa4016aa7fd35734834
Tobias Rødahl Thingnes <tobiasth@stud.ntnu.no>
2021-05-11 12:10:44 +0200

    Add logo image

--- src/main/java/edu/ntnu/tobiasth/idatt2001/postalregistry/App.java
+++ src/main/java/edu/ntnu/tobiasth/idatt2001/postalregistry/App.java
@@ -1,8 +1,11 @@
 package edu.ntnu.tobiasth.idatt2001.postalregistry;
 
+import java.io.InputStream;
+import java.util.Objects;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -13,13 +16,18 @@ import javafx.stage.Stage;
 public class App extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
-    var loader = new FXMLLoader(getClass().getResource("/app.fxml"));
+    var loader = new FXMLLoader(App.class.getResource("/app.fxml"));
     var scene = new Scene(loader.load());
+
+    try {
+      InputStream imageStream = App.class.getResourceAsStream("/logo.png");
+      primaryStage.getIcons().add(new Image(Objects.requireNonNull(imageStream)));
+    } catch (NullPointerException ignored) {
+      /* No icon is added if load fails */
+    }
 
     primaryStage.setScene(scene);
     primaryStage.setTitle("Postal Code Register");
-    primaryStage.setMinWidth(1200);
-    primaryStage.setMinHeight(800);
     primaryStage.centerOnScreen();
     primaryStage.show();
   }
